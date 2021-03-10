cf6ffd26b15dfe14d289e9c1875b9ea37751cbe6
user <user@DESKTOP-74S7IB0>
2021-03-10 01:08:21 +0100

    under dev

--- tstGIt/src/tictactoe_Game/tictactoe.java
+++ tstGIt/src/tictactoe_Game/tictactoe.java
@@ -1,17 +1,17 @@
 package tictactoe_Game;
 
-
 import java.util.ArrayList;
 import java.util.List;
-
-import FX_TST.Sign_In;
+import java.util.Optional;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -23,11 +23,11 @@ import javafx.stage.Stage;
 public class tictactoe extends Application {
 	   private boolean used_O,used_X ; //to prevent playing X or O in a row
 	   private int count=0,id=0;
-	   
 	   public  ArrayList<Container> boxes = new ArrayList<>();
+	   private Stage stage0=new Stage();
 	   List<Integer> O_box=new ArrayList();
 	   List<Integer> X_box=new ArrayList();
-	   
+	   private boolean draw=true;
 	   /* function to create  scene */
 	      private Scene create_scene(double height,double width) {
 		  Pane root= new Pane();
@@ -51,9 +51,8 @@ public class tictactoe extends Application {
 	  
 	  
 	@Override
-	public void start(Stage stage0) throws Exception {
+	public void start(Stage stage0x) throws Exception {
 		stage0.setScene(create_scene(500,500) );
-
 		stage0.setTitle("TIC_TAC_TOE");
 		stage0.show();
 	}
@@ -63,6 +62,7 @@ public class tictactoe extends Application {
 		private Label label =new Label();
 		private Button btnO = new Button("0");
 	    private Button btnX = new Button("X");
+	    private Button play =new Button("Play Again");
 	    private int id_box;
 	    private int title=2; //title = 0 if is a O   and =1 if a X
 	    private boolean used=false ;//to block multiple changements of same box
@@ -102,11 +102,10 @@ public class tictactoe extends Application {
 		        	used_O=true;
 		        	used_X=false;
 		        	fill_boxes();
-		        	Draw();
 		        	count++;
 		        	win_X();
 		        	win_O();
-		        	
+		        	Draw();
 		            
 		        }
 		    });
@@ -125,11 +124,10 @@ public class tictactoe extends Application {
 	            used_X=true;
 	            used_O=false;
 	            fill_boxes();
-	            Draw();
 	            count++;
 	            win_X();
 	        	win_O();
-	        	
+	            Draw();     	
 	           
 	        }
 	    });
@@ -163,6 +161,22 @@ public class tictactoe extends Application {
 	  }
 	}
 	
+ 
+	
+/********************************PLAY AGAIN*******************************/	
+ public void restart() {
+	 stage0.close();
+	 tictactoe window = new tictactoe();
+	 Stage stage = new Stage();
+	 try {
+		window.start(stage);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} 
+	 }
+    	
+	
 /********************************WIN**************************************/	
 public void win_O() {
     boolean win0=O_box.contains(4) && O_box.contains(0) && O_box.contains(8);
@@ -171,75 +185,112 @@ public void win_O() {
     boolean win3=O_box.contains(4) && O_box.contains(3) && O_box.contains(5);    
 		
     if(win0 || win1 || win2 || win3) {
+    	draw=false;
     	System.out.print("WINNER IS O!");
-    	winning window = new winning();
-		try {
-			window.start(new Stage());
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-		}
+    // create a alert 
+         Alert a = new Alert(AlertType.NONE);  
+        a.setAlertType(AlertType.INFORMATION); 
+        a.setHeaderText(null);
+        a.setGraphic(null);
+        a.setContentText("PLAYER_O WINS!!!");
+    //show  alert 
+         
+        Optional<ButtonType> result = a.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+           restart();
+        }
+        
 		
-	}
-	  
-    
+    }
+	
      
      win0= O_box.contains(0) && O_box.contains(1) && O_box.contains(2);
 	 win1= O_box.contains(0) && O_box.contains(3) && O_box.contains(6);	
 	 win2=O_box.contains(2) && O_box.contains(5) && O_box.contains(8);
 	 win3=O_box.contains(6) && O_box.contains(7) && O_box.contains(8);
      if( win0 || win1 || win2) {
+    	 draw=false;
      System.out.print("WINNER IS O!");
- 	winning window = new winning();
-		try {
-			window.start(new Stage());
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-		}
-     
-     
+  // create a alert 
+     Alert a = new Alert(AlertType.NONE);  
+    a.setAlertType(AlertType.INFORMATION); 
+    a.setHeaderText(null);
+    a.setGraphic(null);
+    a.setContentText("PLAYER_O WINS!!!");
+    //show   
+    Optional<ButtonType> result = a.showAndWait();
+
+    if (result.get() == ButtonType.OK) {
+       restart();
+    }
      }
-		}
+     
+ 	}
+
+
 public void win_X() {
-	
     boolean win0=X_box.contains(4) && X_box.contains(0) && X_box.contains(8);
     boolean win1=X_box.contains(4) && X_box.contains(1) && X_box.contains(7);
     boolean win2=X_box.contains(4) && X_box.contains(2) && X_box.contains(6);
     boolean win3=X_box.contains(4) && X_box.contains(3) && X_box.contains(5);    
-	
+
      if(win0 || win1 || win2 || win3) {
+    	 draw=false;
     	 System.out.print("WINNER IS X!");
-     	winning window = new winning();
- 		try {
- 			window.start(new Stage());
- 		} catch (Exception e) {
- 			
- 			e.printStackTrace();
- 		}
-    	 
+    	// create a alert 
+         Alert a = new Alert(AlertType.NONE);  
+        a.setAlertType(AlertType.INFORMATION); 
+        a.setHeaderText(null);
+        a.setGraphic(null);
+        a.setContentText("PLAYER_X WINS!!!");
+        //show   
+        Optional<ButtonType> result = a.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+           restart();
+        }
+		
      }
-	
      win0= X_box.contains(0) && X_box.contains(1) && X_box.contains(2);
 	 win1= X_box.contains(0) && X_box.contains(3) && X_box.contains(6);	
      win2=X_box.contains(2) && X_box.contains(5) && X_box.contains(8); 
      win3=X_box.contains(6) && X_box.contains(7) && X_box.contains(8); 
      
-     if(win0 || win1 || win2 || win3) { 
+     if(win0 || win1 || win2 || win3) {
+    	 draw=false;
     	 System.out.print("WINNER IS X!");
-     	winning window = new winning();
- 		try {
- 			window.start(new Stage());
- 		} catch (Exception e) {
- 			
- 			e.printStackTrace();
- 		}
-     
+    	// create a alert 
+         Alert a = new Alert(AlertType.NONE);  
+        a.setAlertType(AlertType.INFORMATION); 
+        a.setHeaderText(null);
+        a.setGraphic(null);
+        a.setContentText("PLAYER_X WINS!!!");
+        //show   
+        Optional<ButtonType> result = a.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+           restart();
+        }
+		
      }
       }		
 /******************************DRAW****************************************/	
 	public void Draw() {
-		if(count==8 ) System.out.print("DRAW");//here display DRAW NO WINNER
+		if(count==9 && draw) {
+			System.out.print("DRAW");//here display DRAW NO WINNER
+			Alert a = new Alert(AlertType.NONE);  
+	        a.setAlertType(AlertType.INFORMATION); 
+	        a.setHeaderText(null);
+	        a.setGraphic(null);
+	        a.setContentText("DRAW!!!");
+	        //show   
+	        Optional<ButtonType> result = a.showAndWait();
+
+	        if (result.get() == ButtonType.OK) {
+	           restart();
+	        }
+		}
 	}
 /***************************************************************************/	
 	public static void main(String[] args) {
--- tstGIt/src/tictactoe_Game/winning.java
+++ tstGIt/src/tictactoe_Game/winning.java
@@ -1,36 +1,34 @@
 package tictactoe_Game;
 
-import java.util.Optional;
+
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class winning extends Application{
 
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		GridPane grille = new GridPane();
-		Scene scene= new Scene(grille,500,500);
-		
-//********************Event Button Quitter********************************	
-		
-		stage.setScene(scene);
-		stage.setTitle("CONGRATS");
-		stage.show();
-		
-	}
+		 @Override
+	    public void start(Stage primaryStage) {
+	        StackPane root = new StackPane();
+	        root.setId("pane");
+	        BackgroundImage myBI= new BackgroundImage(new Image("D:\\pic",32,32,false,true),
+	                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+	                  BackgroundSize.DEFAULT);
+	        //then you set to your node
+	        root.setBackground(new Background(myBI));
+	        Scene scene = new Scene(root, 300, 250);
+	        primaryStage.setScene(scene);
+	        
+	        primaryStage.show();
+	    }
 	
 	public static void main(String[] args) {
 	launch(args);
