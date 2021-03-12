27645bf35f945ab3b50d64b1d24b9a066809d3bc
user <user@DESKTOP-74S7IB0>
2021-03-12 23:47:09 +0100

    under dev

--- ttest/src/application/Main.java
+++ ttest/src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application   {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("application/Ddd.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Fracken");
+        stage.setTitle("TIC_TAC_TOE");
         stage.show();
         
     }
